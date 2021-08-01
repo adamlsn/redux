3f65ceb8955c4bf4aef21c55af08003aaaf1dcac
Adam Olson <adamlsn@gmail.com>
2021-08-01 17:35:13 -0500

    trying to resolve heroku errors, images missing

---
+++ client/build/precache-manifest.29f4b91127f42bc121c3ee098887963f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1587d377e2420c273617c54783ad2dbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bfff329d6f1c462a248",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "0da072bf0c73c51c54c2",
+    "url": "/static/js/2.4ba422bb.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.4ba422bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bfff329d6f1c462a248",
+    "url": "/static/js/main.1ac9ff3f.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
